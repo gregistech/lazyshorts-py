5cc328101c7e264deb7b904174ee1cf664ea0ede
gergovari <work@varigergo.xyz>
2022-12-27 10:31:11 +0100

    refactor: working directory management

--- pipelinehandler.py
+++ pipelinehandler.py
@@ -14,33 +14,70 @@ from moviepy.video.fx.all import crop
 import tempfile
 import shutil
 
+import uuid
+from pathlib import Path
+import os
+class WorkingDirectoryManager:
+    def __init__(self, work_dir = False):
+        self._work_dir = work_dir
+        self._is_work_dir_created = work_dir
+        self._is_work_dir_set_by_user = work_dir
+        self._registered_files = []
+
+    @property
+    def work_dir(self):
+        if not self._is_work_dir_created:
+            if self._is_work_dir_set_by_user:
+                Path(self._work_dir).mkdir(parents=True, exist_ok=True)
+            else:
+                self.work_dir = tempfile.mkdtemp() 
+        return self._work_dir
+    @work_dir.setter
+    def work_dir(self, work_dir):
+        self._work_dir = work_dir + "/"
+
+    def __del__(self):
+        self._cleanup_work_dir()
+
+    def _delete_registered_files(self):
+        for file in self._registered_files:
+            os.remove(file)
+
+    def _cleanup_work_dir(self):
+        if self._is_work_dir_set_by_user:
+            PipelineHandler._delete_registered_files()
+        else:
+            shutil.rmtree(self.work_dir)
+
+    def create_file(self, file):
+        new_file = self.work_dir + str(uuid.uuid4()) + file
+        self._registered_files.append(new_file)
+        return new_file
+
 class PipelineHandler:
-    def __init__(self, original_video_file = False, work_dir = False):
+    def __init__(self, original_video_file = False):
         self._video = ""
         self.segments = ""
         self._original_video_file = ""
         self.transcriber = Transcriber()
-        self._work_dir = work_dir
-        self._is_work_dir_set = work_dir
+        self.work_dir_handler = WorkingDirectoryManager()
         if original_video_file:
             self.set_original_video_file(original_video_file)
     
-    def __del__(self):
-        self._cleanup_work_dir()
-
     def _cut_silence(self, video):
-        fast = self.work_dir + "/fast.mp4"
-        subprocess.run(["auto-editor", video, "--no-open", "-o", fast],
+        fast = self.work_dir_handler.create_file("file.mp4")
+        # FIXME: temp dir is not picked up (it works on the command-line...)
+        proc = subprocess.run(["auto-editor", video, "--no-open", "--temp-dir", self.work_dir_handler.work_dir, "-o", fast],
                 stdout=subprocess.DEVNULL,
                 stderr=subprocess.STDOUT)
+        print(proc)
         return fast
     def _v_to_a(self, video):
         clip = VideoFileClip(video)
-        audio = self.work_dir + "/audio.wav"
+        audio = self.work_dir_handler.create_file("audio.wav")
         clip.audio.write_audiofile(audio, logger=None)
         return audio
     def _preprocess_video(self, original_video):
-        self._work_dir = self._get_current_work_dir()
         video = self._cut_silence(original_video)
         audio = self._v_to_a(video)
         return video, audio
@@ -49,14 +86,6 @@ class PipelineHandler:
         video, audio = self._preprocess_video(video)
         return video, self.transcriber.audio_to_segments(audio)
  
-    @property
-    def work_dir(self):
-        return self._work_dir
-    @work_dir.setter
-    def work_dir(self, work_dir):
-        self._is_work_dir_set = True
-        self._work_dir = work_dir
-
     @property
     def original_video_file(self):
         return self._original_video_file
@@ -104,41 +133,24 @@ class PipelineHandler:
         (w, h) = clip.size
         return crop(clip, x_center = w/2, y_center = h/2, width = 607, height = 1080)
     def _clip_to_file(self, clip):
-        clip_file = self.work_dir + "/end.mp4"
+        clip_file = self.work_dir_handler.create_file("end.mp4")
         clip.write_videofile(clip_file, logger=None) 
         return clip_file
     def _overlay_end_text_on_clip(clip, end_time = 5):
         return concatenate_videoclips([clip.subclip(0, clip.end - end_time), CompositeVideoClip([clip.subclip(clip.end - end_time, clip.end), TextClip("A teljes videó megtalálható fő csatornáimon!", fontsize = 48, method = "caption", font = "Arial", color="white").set_duration(end_time).set_pos("center", "center")])])
 
     def _burn_in_subs_to_file(self, file, sub_file):
-        subbed_file = self.work_dir + "/subbed.mp4"
+        subbed_file = self.work_dir_handler.create_file("subbed.mp4")
         subprocess.run(["ffmpeg", "-y", "-i", file, "-vf", f"subtitles={sub_file}:force_style='Alignment=2,MarginV=50,Fontsize=12'", "-c:a", "copy", subbed_file])#,
             #stdout=subprocess.DEVNULL,
             #stderr=subprocess.STDOUT)
         return subbed_file
     def _subs_to_file(self, subs):
-        sub_file = self.work_dir + "/subs.srt"
+        sub_file = self.work_dir_handler.create_file("subs.srt")
         with open(sub_file, "w") as file:
             file.write(srt.compose(subs))
         return sub_file
     
-    def _get_current_work_dir(self):
-        if self._is_work_dir_set:
-            return self.work_dir
-        else:
-            return tempfile.mkdtemp()
-    def _delete_dir_contents(directory):
-        for root, dirs, files in os.walk(directory):
-            for f in files:
-                os.unlink(os.path.join(root, f))
-            for d in dirs:
-                shutil.rmtree(os.path.join(root, d))
-    def _cleanup_work_dir(self):
-        if not self._is_work_dir_set:
-            shutil.rmtree(self.work_dir)
-        else:
-            PipelineHandler._delete_dir_contents(self.work_dir)
-
     def render_file(self, selected, final_file):
         concat, subs = self._segments_to_clip_and_subs(selected)
         cropped = PipelineHandler._crop_clip(concat)
