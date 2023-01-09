9162eaa6a78daec4650b23ccb553b3da4b5b23e5
gergovari <work@varigergo.xyz>
2023-01-09 14:41:14 +0100

    misc: bad dsit

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = fh.read()
+with open("./requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read();
     setup(
         name = 'lazyshorts-py', # FIXME: sync with __main__.py
-        version = '0.0.1',
+        version = '0.0.10',
         author = 'Gergő Vári',
         author_email = 'work@varigergo.xyz',
         license = 'GNU LESSER GENERAL PUBLIC LICENSE 3',
@@ -16,14 +16,11 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
         long_description_content_type = "text/markdown",
         url = 'https://github.com/gergovari/lazyshorts-py',
         packages = find_packages(),
-        install_requires = [requirements],
+        install_requires = requirements,
         python_requires='>=3.7',
         classifiers=[
             "Programming Language :: Python :: 3.8",
             "Operating System :: OS Independent",
-        ],
-        dependency_links = [
-            "https://download.pytorch.org/whl/torch_stable.html"
         ],
         entry_points = '''
             [console_scripts]
